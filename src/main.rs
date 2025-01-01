2dd1b084fd229217e466a1382c18a88486092f69
Moss Cantwell <mossman2142@gmail.com>
2025-01-01 16:41:10 +1300

    yay it makes obj's

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use shapefile::record::EsriShape;
 use shapefile::{Shape, NO_DATA};
 use shapefile::PointZ;
 use wavefront_rs::obj::entity::Entity::Vertex;
-use wavefront_rs::obj::{self, entity::Entity};
+use wavefront_rs::obj::{self, entity::Entity, format_writer::FormatWriter};
 use std::borrow::Borrow;
 use std::fmt::Debug;
+use std::fs::File;
 use std::{error::Error, io::Write, str::FromStr};
 
 #[derive(Parser)]
@@ -30,7 +31,12 @@ struct Arguments {
 fn main() -> std::io::Result<()> {
     let args = Arguments::parse();
     let mut output = args.outfile.create()?;
-    writeln!(&mut output, "{:?}", convert_mesh(args.infile))?;
+    let mesh = convert_mesh(args.infile);
+    for vertex in  mesh{
+        FormatWriter::write(output.by_ref(), &vertex);
+        writeln!(output.by_ref())?;
+    }
+
     Ok(())
 }
 
@@ -47,7 +53,7 @@ fn main() -> std::io::Result<()> {
  *   save to a new vertex in mutable wavefront entity
  * return entity or a copy of entity
  */
-fn convert_mesh (inputfile: InputArg) -> Result<Entity,String>{
+fn convert_mesh (inputfile: InputArg) -> Vec<Entity>{
     // create a mutable wavefront entity collection
     let mut wavefront_ent = Vec::new();
     let mut reader = shapefile::Reader::from_path(inputfile.into_path().expect("input path failed")).expect("failed to create reader");
@@ -56,15 +62,15 @@ fn convert_mesh (inputfile: InputArg) -> Result<Entity,String>{
     let point_zero = shapefile::PointZ::try_from(shape_zero).expect("failed to convert to pointZ");
     for shape_record in  readervec{
         let (shape, record) = shape_record.expect("failed to make shape record");
-        println!("shape: {}", shape);
+        //println!("shape: {}", shape);
         let pointy = shapefile::PointZ::try_from(shape).expect("cannot convert to pointZ");
         let delta_point = distance_calculation(point_zero, pointy);
         let vert = Vertex{x: delta_point.x, y: delta_point.y, z:delta_point.z,w: None};
-        println!("vert: {}",vert.to_string());
+        //println!("vert: {}",vert.to_string());
         wavefront_ent.push(vert);
     }
 
-    Err(String::from("not implemented"))
+    wavefront_ent
 }
 /**
  * calculate component distances from pt1 to pt2 from lon,lat (hopefully into meters?)
